f7eaef24d4abc76d7ce91dc41c7e65b7c40e5e7d
VasiliyZykin <obliviont121@gmail.com>
2023-07-12 02:08:16 +0300

    dz5

---
+++ hw-game.js
@@ -0,0 +1,30 @@
+function monthNumberGame() {
+    let monthNumber = String(prompt('Введите номер месяца'));
+
+    switch (monthNumber) {
+        case '12':
+        case '1':
+        case '2':
+            console.log('зима');
+            break;
+        case '3':
+        case '4':
+        case '5':
+            console.log('весна');
+            break;
+        case '6':
+        case '7':
+        case '8':
+            console.log('лето');
+            break;
+        case '9':
+        case '10':
+        case '11':
+            console.log('осень');
+            break;
+
+        default:
+            console.log('такого месяца не бывает')
+            break;
+    }
+}
